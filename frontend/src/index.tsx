9beb72274496186daba127e3fa27bf95f6e6cca6
0xJupiter <junta8918@gmail.com>
2022-08-22 14:40:23 +0900

    add react router, dotenv, etc

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,52 +1,52 @@
-import { ChakraProvider, Container, HStack, Spinner, Stack, Text } from "@chakra-ui/react"
+import { ChakraProvider, Button, Container, HStack, Spinner, Stack, Text, Heading } from "@chakra-ui/react"
 import "@fontsource/inter/400.css"
 import detectEthereumProvider from "@metamask/detect-provider"
 import { Identity } from "@semaphore-protocol/identity"
 import { Contract, providers, Signer } from "ethers"
 import { hexlify } from "ethers/lib/utils"
-import { useEffect, useState } from "react"
+import { useEffect, useState, useMemo } from "react"
 import { createRoot } from "react-dom/client"
-import Events from "../../contracts/build/contracts/contracts/ZkGateway.sol/ZkGateway.json"
+import Gateway from "../../contracts/build/contracts/contracts/ZkGateway.sol/ZkGateway.json"
 import theme from "../styles"
-import GroupStep from "./components/GroupStep"
-import IdentityStep from "./components/IdentityStep"
-import ProofStep from "./components/ProofStep"
+import { BrowserRouter as Router, Link, useLocation } from "react-router-dom"
+import "dotenv/config"
 
 function App() {
-    const [_logs, setLogs] = useState<string>("")
-    const [_step, setStep] = useState<number>(1)
     const [_identity, setIdentity] = useState<Identity>()
     const [_signer, setSigner] = useState<Signer>()
     const [_contract, setContract] = useState<Contract>()
-    const [_event, setEvent] = useState<any>()
+    const [_account, setAccount] = useState<string>("")
+    const [_userId, setUserId] = useState<string>("")
+    const [_guildId, setGuildId] = useState<string>("")
+
+    // TODO: extract to constants
+    const contractAddress = "0xBA6BfBa894B5cAF04c3462A5C8556fFBa4de6782"
+    const discordApiURL = "https://discord.com/api/v10/"
+
+    function useQuery() {
+        const { search } = useLocation()
+
+        return useMemo(() => new URLSearchParams(search), [search])
+    }
+
+    let query = useQuery()
 
     useEffect(() => {
         ;(async () => {
-            const ethereum = (await detectEthereumProvider()) as any
-            const accounts = await ethereum.request({ method: "eth_requestAccounts" })
+            const userId = query.get("userId")
+            setUserId(userId!)
+            const guildId = query.get("guildId")
+            setGuildId(guildId!)
 
-            await ethereum.request({
-                method: "wallet_switchEthereumChain",
-                params: [
-                    {
-                        chainId: hexlify(Number(process.env.ETHEREUM_CHAIN_ID!)).replace("0x0", "0x")
-                    }
-                ]
-            })
+            const ethereum = (await detectEthereumProvider()) as any
 
             const ethersProvider = new providers.Web3Provider(ethereum)
-
-            if (accounts[0]) {
-                setSigner(ethersProvider.getSigner())
-
-                setContract(new Contract(process.env.CONTRACT_ADDRESS!, Events.abi, ethersProvider.getSigner()))
-            }
 
             ethereum.on("accountsChanged", (newAccounts: string[]) => {
                 if (newAccounts.length !== 0) {
                     setSigner(ethersProvider.getSigner())
 
-                    setContract(new Contract(process.env.CONTRACT_ADDRESS!, Events.abi, ethersProvider.getSigner()))
+                    setContract(new Contract(contractAddress, Gateway.abi, ethersProvider.getSigner()))
                 } else {
                     setSigner(undefined)
                 }
@@ -54,49 +54,64 @@ function App() {
         })()
     }, [])
 
+    const connectWallet = async () => {
+        const ethereum = (await detectEthereumProvider()) as any
+        const accounts = await ethereum.request({ method: "eth_requestAccounts" })
+
+        // await ethereum.request({
+        //     method: "wallet_switchEthereumChain",
+        //     params: [
+        //         {
+        //             chainId: hexlify(Number(1666900000)).replace("0x0", "0x")
+        //         }
+        //     ]
+        // })
+
+        const ethersProvider = new providers.Web3Provider(ethereum)
+
+        console.log(ethersProvider)
+
+        if (accounts[0]) {
+            setAccount(accounts[0])
+            setSigner(ethersProvider.getSigner())
+
+            setContract(new Contract(contractAddress, Gateway.abi, ethersProvider.getSigner()))
+            const guildId = query.get("guildId")
+
+            const res = await fetch(`${discordApiURL}/guilds/${guildId}`, {
+                method: "GET",
+                headers: { Authorization: `Bot ${process.env.discordApiURL}` }
+            })
+
+            console.log(res)
+        }
+    }
+
     return (
         <>
             <Container maxW="lg" flex="1" display="flex" alignItems="center">
                 <Stack>
-                    {_step === 1 ? (
-                        <IdentityStep onChange={setIdentity} onLog={setLogs} onNextClick={() => setStep(2)} />
-                    ) : _step === 2 ? (
-                        <GroupStep
-                            signer={_signer}
-                            contract={_contract}
-                            identity={_identity as Identity}
-                            onPrevClick={() => setStep(1)}
-                            onSelect={(event) => {
-                                setEvent(event)
-                                setStep(3)
-                            }}
-                            onLog={setLogs}
-                        />
-                    ) : (
-                        <ProofStep
-                            signer={_signer}
-                            contract={_contract}
-                            identity={_identity as Identity}
-                            event={_event}
-                            onPrevClick={() => setStep(2)}
-                            onLog={setLogs}
-                        />
-                    )}
+                    <Heading as="h2" size="xl">
+                        Verify
+                    </Heading>
+                    <Text pt="2" fontSize="md">
+                        Verify your NFT holding to access closed channel.
+                    </Text>
+                    <Text pt="2" fontSize="md">
+                        Wallet address: {_account}
+                    </Text>
+                    <Button
+                        w="100%"
+                        fontWeight="bold"
+                        justifyContent="left"
+                        colorScheme="primary"
+                        px="4"
+                        onClick={connectWallet}
+                    >
+                        Connect Wallet
+                    </Button>
                 </Stack>
             </Container>
-
-            <HStack
-                flexBasis="56px"
-                borderTop="1px solid #8f9097"
-                backgroundColor="#DAE0FF"
-                align="center"
-                justify="center"
-                spacing="4"
-                p="4"
-            >
-                {_logs.endsWith("...") && <Spinner color="primary.400" />}
-                <Text fontWeight="bold">{_logs || `Current step: ${_step}`}</Text>
-            </HStack>
         </>
     )
 }
@@ -105,6 +120,8 @@ const root = createRoot(document.getElementById("app")!)
 
 root.render(
     <ChakraProvider theme={theme}>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </ChakraProvider>
 )
