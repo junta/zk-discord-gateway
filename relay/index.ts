256046a9ed60ebc96b1181ac467fea4375f2b182
0xJupiter <junta8918@gmail.com>
2022-08-23 18:20:25 +0900

    update relay index

--- relay/index.ts
+++ relay/index.ts
@@ -1,11 +1,9 @@
 import cors from "cors"
-import { config as dotenvConfig } from "dotenv"
+import "dotenv/config"
 import { Contract, providers, utils, Wallet } from "ethers"
 import express from "express"
 import { resolve } from "path"
-import { abi as contractAbi } from "../contracts/build/contracts/contracts/Events.sol/Events.json"
-
-dotenvConfig({ path: resolve(__dirname, "../../.env") })
+import { abi as contractAbi } from "../contracts/build/contracts/contracts/ZkGateway.sol/ZkGateway.json"
 
 if (typeof process.env.CONTRACT_ADDRESS !== "string") {
     throw new Error("Please, define CONTRACT_ADDRESS in your .env file")
@@ -19,14 +17,10 @@ if (typeof process.env.ETHEREUM_PRIVATE_KEY !== "string") {
     throw new Error("Please, define ETHEREUM_PRIVATE_KEY in your .env file")
 }
 
-if (typeof process.env.RELAY_URL !== "string") {
-    throw new Error("Please, define RELAY_URL in your .env file")
-}
-
 const ethereumPrivateKey = process.env.ETHEREUM_PRIVATE_KEY
 const ethereumURL = process.env.ETHEREUM_URL
 const contractAddress = process.env.CONTRACT_ADDRESS
-const { port } = new URL(process.env.RELAY_URL)
+const port = 3000
 
 const app = express()
 
@@ -75,5 +69,5 @@ app.post("/add-member", async (req, res) => {
 })
 
 app.listen(port, () => {
-    console.info(`Started HTTP relay API at ${process.env.RELAY_URL}/`)
+    console.info("Started HTTP relay API")
 })
